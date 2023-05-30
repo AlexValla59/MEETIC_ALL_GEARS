ddf1cdb9f0f0953d9df76d4dca5257636f3b9d58
foucauld blouin <foucauld.blouin@gmail.com>
2023-05-30 14:10:33 +0200

    controller+routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,5 @@ class Booking < ApplicationRecord
   belongs_to :user
 
 # Validations
-  # Ajoutez vos validations ici, par exemple si vous avez un champ date :
   validates :date, presence: true
 end
