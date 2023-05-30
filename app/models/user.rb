e7d269c4d4eece812e1aac5322a04ae1fa8f24f2
FX Lebon <fx.lebon@gmail.com>
2023-05-30 14:51:53 +0200

    ajout users validates et controller user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
 
   # Validations
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  UNIVERS = ["Starwars", "Seigneurs des anneaux", "Harry Potter", "Indiana Jones", "Men In Black", "Doctor who", "Shinnig", "5ème élément", "autres"]
+  validates :univers, inclusion: { in: UNIVERS }
+
 end
